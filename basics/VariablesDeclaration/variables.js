efe23acb699dd250f7f187befc1b4e1a87bc3542
Anjanaph21 <--global>
2026-04-18 23:07:41 +0530

    feat: add basic JavaScript variable declaration examples and HTML boilerplate

---
+++ basics/VariablesDeclaration/variables.js
@@ -0,0 +1,18 @@
+// VARIABLES AND DECLARATION
+//Variables:
+//variables are used to store data in memory
+//3 types : var, let and const
+//var is belongs to ES5
+//let and const belongs to ES6
+// why we need to create variables in our code? -> because we can store data in memory and access it later in future.
+
+//var: Var is a function scoped variable.
+//let: Let is a block scoped variable.
+//const: Const is a block scoped variable.
+
+//var
+var a; // this is called declaration
+var a = 10; // this is called declaration + initialization
+//same goes for let as well
+let b;
+ b = 12;
